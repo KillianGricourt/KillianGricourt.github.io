5af7253251a8799c28777919d3e295e3b9219e3d
Killian Gricourt <gricourt.killian@gmail.com>
2024-12-17 02:38:25 +0100

    Création d'un composant pour expérience.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
