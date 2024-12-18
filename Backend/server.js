339c72d9f795d93859577493f279aece090f76fa
Killian Gricourt <gricourt.killian@gmail.com>
2024-12-18 16:03:45 +0100

    Init backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,43 @@
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
+
+const dataFilePath = path.join(__dirname, 'data', 'experiences.json');
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Start server
+const readData = () => {
+    try {
+        const data = fs.readFileSync(dataFilePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading JSON file:', error);
+        return [];
+    }
+};
+
+//I don't plan to use this for now but at least it's here
+/*const writeData = (data) => {
+    try {
+        fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error writing JSON file:', error);
+    }
+};*/
+
+app.get('/experiences', (req, res) => {
+    const experiences = readData();
+    res.status(200).json(experiences);
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
