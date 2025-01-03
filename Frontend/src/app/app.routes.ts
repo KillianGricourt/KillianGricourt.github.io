0889d52ec04e65f1d1cd41bd821a6d6803823dbc
Killian Gricourt <gricourt.killian@gmail.com>
2025-01-03 20:04:45 +0100

    Modify contact css

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExperienceListComponent } from './experience/experiences.component';
 import { HomeComponent } from './home/home.component';
+import { EasterEggComponent } from './easter-egg/easter-egg.component';
 
 export const routes: Routes = [
   { path: 'experiences', component: ExperienceListComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'easter-egg', component: EasterEggComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirection par défaut
   { path: '**', redirectTo: '/home' }, // Gestion des routes non trouvées
 ];
---
+++ Frontend/src/app/easter-egg/easter-egg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-easter-egg',
+  imports: [],
+  templateUrl: './easter-egg.component.html',
+  styleUrl: './easter-egg.component.css'
+})
+export class EasterEggComponent {
+
+}
