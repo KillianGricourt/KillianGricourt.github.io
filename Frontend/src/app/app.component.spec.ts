0086a141b07e4d87dc417d69ac1ebb2a63cf721a
Killian Gricourt <gricourt.killian@gmail.com>
2025-01-23 14:11:11 +0100

    update spec

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'KillianGricourt' title`, () => {
+  it(`should have the 'gricourt.killian@gmail.com' mail`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KillianGricourt');
+    expect(app.email).toEqual('gricourt.killian@gmail.com');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, KillianGricourt');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Killian Gricourt');
   });
 });
